f7a19624e05ac6d619a671169458473e5f5d2f75
naeem majumder <naeemmajumder00@gmail.com>
2025-02-12 13:26:14 +0600

    complte use axios Secure page

--- src/customHooks/UseAxiosSecure.jsx
+++ src/customHooks/UseAxiosSecure.jsx
@@ -1,11 +1,13 @@
+import axios from 'axios';
 import React from 'react';
 
+
+const axiosSecure = axios.create({
+    baseURL:'http://localhost:3000'
+}) 
+
 const UseAxiosSecure = () => {
-    return (
-        <div>
-            use axios
-        </div>
-    );
+    return axiosSecure;
 };
 
 export default UseAxiosSecure;
